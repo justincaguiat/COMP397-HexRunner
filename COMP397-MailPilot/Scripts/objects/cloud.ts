22576777d7315852e25bd62fe22c2fe2d8288d3d
Justin Caguiat <justin.caguiat@gmail.com>
2015-03-17 19:02:46 -0400

    forgot to git hub.

---
+++ COMP397-MailPilot/Scripts/objects/cloud.ts
@@ -0,0 +1,38 @@
+﻿module objects {
+    // CLOUD CLASS
+    export class Cloud extends objects.GameObject {
+
+        // CONSTRUCTOR
+        constructor() {
+            super("cloud");
+            this.sound = "thunder";
+            this.reset();
+        }
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+        public update() {
+            //this.y += this._dy;
+            this.x -= this._dx;
+
+            this._checkBounds();
+        }
+
+        // Reset position of island to the top
+        public reset() {
+            this.y = Math.floor(Math.random() * 480);;
+            this.x = 690;
+            this._dy = Math.floor(Math.random() * 5) + 5;
+            this._dx = Math.floor(Math.random() * 5) + 5;
+        }
+
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++
+        private _checkBounds() {
+            // check if island has left the bottom of the screen
+            if (this.x<0) {
+                this.reset();
+            }
+        }
+
+    }
+
+}  
---
+++ COMP397-MailPilot/Scripts/objects/island.ts
@@ -0,0 +1,37 @@
+﻿module objects {
+    // ISLAND CLASS
+    export class Island extends objects.GameObject{
+
+        // CONSTRUCTOR
+        constructor() {
+            super("island");
+            this.sound = "yay";
+            this._dy = 5;
+
+            this.reset();
+        }
+
+        // PUBLIC METHODS ++++++++++++++++++++++++++++++++++++++++++
+        public update() {
+            this.x += this._dy;
+
+            this._checkBounds();
+        }
+
+        // Reset position of island to the top
+        public reset() {
+            this.x = -this.width;
+            this.y = Math.floor(Math.random() * 440);
+        }
+
+        // PRIVATE METHODS +++++++++++++++++++++++++++++++++++++++++
+        private _checkBounds() {
+            // check if island has left the bottom of the screen
+            if (this.x >= (640 + this.width)) {
+                this.reset();
+            }
+        }
+
+    }
+
+} 
